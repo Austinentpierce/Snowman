cee57ec469f34532e919d64a97a990ce28c025bf
Austin Pierce <Austinentpierce@gmail.com>
2021-09-13 14:53:45 -0400

    added background and more styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ import step0 from './images/step_0.png'
 export function App() {
   return (
     <main>
+      <div className="wrapper">
+        <div className="snow layer1 a"></div>
+        <div className="snow layer1"></div>
+        <div className="snow layer2 a"></div>
+        <div className="snow layer2"></div>
+        <div className="snow layer3 a"></div>
+        <div className="snow layer3"></div>
+      </div>
       <header>
         <div>The Snowman Game</div>
       </header>
-      <img className="firstimage" src={step0} width="600" height="600"></img>
+
+      <img className="firstimage" src={step0} width="450" height="450"></img>
       <section>
         <ul>
           <li className="firstletter">_</li>
