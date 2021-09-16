336d5ada14d8a9fa59f3448e45042349aad7411d
Austin Pierce <Austinentpierce@gmail.com>
2021-09-16 18:32:01 -0400

    moved some code around

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,53 @@
 import React, { useState } from 'react'
 import step0 from './images/step_0.png'
+import step1 from './images/step_1.png'
+import step2 from './images/step_2.png'
+import step3 from './images/step_3.png'
+import step4 from './images/step_4.png'
+import step5 from './images/step_5.png'
+import step6 from './images/step_6.png'
+import step7 from './images/step_7.png'
+import words from './words.json'
 
-const ALPHABET = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',  
-]
 
 export function App() {
-  
-  const [guessedLetters, setGuessedLetters] = useState([])
+
+  const ALPHABET = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ]
+
+  const randomizedWord = 
+  const [guessedLetters, setGuessedLetters] = useState(['A', 'V'])
   const secretWord = words[0]
 
   console.log(secretWord)
 
-  function clickOnLetter(letter:string) {
+  function clickOnLetter(letter: string) {
     const newValueForGuessedLetters = [...guessedLetters, letter]
 
     setGuessedLetters(newValueForGuessedLetters)
@@ -79,50 +89,21 @@ export function App() {
           <div>{guessedLetters}</div>
         </ul>
       </section>
-        {ALPHABET.map(function () {
-          return (
-            <button key={letter} onClick={function () {
+      {ALPHABET.map(function () {
+        return (
+          <button
+            key={letter}
+            onClick={function () {
               clickOnLetter(letter)
             }}
             disabled={guessedLetters.includes(letter)}
-            > 
+          >
             {letter}
-            </button>
-          )
-        }
-        }
-      
-{/* 
-      <section className="buttons">
-        <button>A</button>
-        <button>B</button>
-        <button>C</button>
-        <button>D</button>
-        <button>E</button>
-        <button>F</button>
-        <button>G</button>
-        <button>H</button>
-        <button>I</button>
-        <button>J</button>
-        <button>K</button>
-        <button>L</button>
-        <button>M</button>
-        <button>N</button>
-        <button>O</button>
-        <button>P</button>
-        <button>Q</button>
-        <button>R</button>
-        <button>S</button>
-        <button>T</button>
-        <button>U</button>
-        <button>V</button>
-        <button>W</button>
-        <button>X</button>
-        <button>Y</button>
-        <button>Z</button>
-      </section>
-
-      <footer>Created by Austin Pierce</footer> */}
+          </button>
+        )
+      })}
     </main>
   )
 }
+
+
